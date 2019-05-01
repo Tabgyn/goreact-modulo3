eb743f286c738a08b3d11e3477ffa5d6fcc56595
Tiago Azevedo Borges <tabgyn@gmail.com>
2019-05-01 11:05:44 -0300

    configured redux

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,71 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
 
-const Main = () => <h1>Main</h1>;
+import * as FavoriteActions from '../../store/actions/favorites';
 
-export default Main;
+class Main extends Component {
+  state = {
+    repositoryInput: '',
+  };
+
+  handleAddRepository = (e) => {
+    e.preventDefault();
+
+    const { addFavorite } = this.props;
+    addFavorite();
+  };
+
+  render() {
+    const { repositoryInput } = this.state;
+    const { favorites } = this.props;
+
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuário/repositório"
+            value={repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Add</button>
+        </form>
+
+        <ul>
+          {favorites.map(favorite => (
+            <li key={favorite.id}>
+              <p>
+                <strong>{favorite.name}</strong> ({favorite.description})
+              </p>
+              <a href={favorite.url}>Acessar</a>
+            </li>
+          ))}
+        </ul>
+      </Fragment>
+    );
+  }
+}
+
+Main.propTypes = {
+  addFavorite: PropTypes.func.isRequired,
+  favorites: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      description: PropTypes.string,
+      url: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+const mapStateToProps = state => ({
+  favorites: state.favorites,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(FavoriteActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Main);
